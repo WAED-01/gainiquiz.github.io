d6ede3f68be7ea92312f288a55aa2d48255b1652
WAED-01 <157380229+WAED-01@users.noreply.github.com>
2024-01-28 06:09:00 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,117 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the largest planet in our solar system?",
+        answer: "B) Jupiter",
+        options: ["A) Mars",
+            "B) Jupiter",
+            "C) Venus",
+            "D) Saturn"
+
+        ],
+        backgroundImage: 'url("planets.jpg")'
+    },
+    {
+        numb: 2,
+        question: "Who painted the Mona Lisa?",
+        answer: "B) Leonardo da Vinci",
+        options: ["A) Vincent van Gogh",
+            "B) Leonardo da Vinci",
+            "C) Pablo Picasso",
+            "D) Claude Monet"
+        ],
+        backgroundImage: 'url("monalisa.jpeg")'
+    },
+    {
+        numb: 3,
+        question: "What is the world's largest ocean?",
+        answer: "D) Pacific Ocean",
+        options: ["A) Atlantic Ocean",
+            "B) Indian Ocean",
+            "C) Southern Ocean",
+            "D) Pacific Ocean"
+        ],
+        backgroundImage: 'url("ocean.jpg")'
+    },
+    {
+        numb: 4,
+        question: "Which country is known as the 'Land of the Midnight Sun'?",
+        answer: "B) Norway",
+        options: ["A) Sweden",
+            "B) Norway",
+            "C) Canada",
+            "D) Russia"
+
+        ],
+        backgroundImage: 'url("norway.jpg")'
+    },
+    {
+        numb: 5,
+        question: "What is the longest river in the world?",
+        answer: "A) Nile",
+        options: ["A) Nile",
+            "B) Amazon",
+            "C) Yangtze",
+            "D) Mississippi"
+
+        ],
+        backgroundImage: 'url("river.jpg")'
+    },
+    {
+        numb: 6,
+        question: "What is the largest mammal in the world?",
+        answer: "B) Blue Whale",
+        options: ["A) Elephant",
+            "B) Blue Whale",
+            "C) Giraffe",
+            "D) Polar Bear"
+        ],
+        backgroundImage: 'url("animal.jpg")'
+    },
+    {
+        numb: 7,
+        question: "What is the currency of Japan?",
+        answer: "B) Yen",
+        options: ["A) Yuan",
+            "B) Yen",
+            "C) Won",
+            "D) Ringgit"
+        ],
+        backgroundImage: 'url("japan.jpg")'
+    },
+    {
+        numb: 8,
+        question: "Who wrote 'Romeo and Juliet'?",
+        answer: "D) William Shakespeare",
+        options: ["A) Charles Dickens",
+            "B) Jane Austen",
+            "C) Mark Twain",
+            "D) William Shakespeare"
+        ],
+        backgroundImage: 'url("romeo.jpg")'
+    },
+    {
+        numb: 9,
+        question: "What is the capital of Australia?",
+        answer: "C) Canberra",
+        options: ["A) Sydney",
+            "B) Melbourne",
+            "C) Canberra",
+            "D) Brisbane"
+
+        ],
+        backgroundImage: 'url("australia.jpg")'
+    },
+    {
+        numb: 10,
+        question: "What is the chemical symbol for gold?",
+        answer: "B) Au",
+        options: ["A) Ag",
+            "B) Au",
+            "C) Fe",
+            "D) Hg"
+
+        ],
+        backgroundImage: 'url("gold.jpg")'
+    }
+];
